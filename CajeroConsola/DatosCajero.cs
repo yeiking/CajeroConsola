e36a5a09ea26ff392c7fd8c357b0a6ff44ad73b3
Yeison Garcia Santos <yeison.santos@outlook.com>
2018-11-25 20:07:23 -0500

    Se corrige el buscador Dni

--- CajeroConsola/DatosCajero.cs
+++ CajeroConsola/DatosCajero.cs
@@ -8,5 +8,31 @@ namespace CajeroConsola
 {
     public class DatosCajero
     {
+
+        DBCajeroConsola db = new DBCajeroConsola();
+        //string CadenaDni;
+        public void BuscarDni()
+        {
+            Console.WriteLine("Ingresa el DNI:");
+            string CadenaDni = Console.ReadLine();
+            Console.WriteLine();
+
+            if (CadenaDni != null)
+            {
+                var searchDNI = from dni in db.TblCliente
+                                where dni.Dni == CadenaDni
+                                select dni;
+
+                foreach (var item in searchDNI)
+                {
+
+                    Console.WriteLine(item.Dni + " " + item.NombreCliente);
+                }
+            }
+        }
+
+
+
+
     }
 }
--- CajeroConsola/Program.cs
+++ CajeroConsola/Program.cs
@@ -16,32 +16,25 @@ namespace CajeroConsola
             Console.WriteLine("Selecciona una opcion:" + "\n");
             Console.WriteLine("1. Buscar cliente." + "\n" + "2. Registrar cliente." + "\n" + "3. Cobrar sin registrar." + "\n" + "4. Clientes Registrados." + "\n");
 
-            DBCajeroConsola db = new DBCajeroConsola();
-            string CadenaDni;
-            switch (Console.Read())
+            DatosCajero operacion = new DatosCajero();
+
+            int op;
+            op = int.Parse(Console.ReadLine());
+
+            switch (op)
             {
-                case '1':
-                    Console.WriteLine("Ingresa el DNI:");
-                    CadenaDni = Console.ReadLine();
+                case 1:
                     
-                    var searchDNI = from dni in db.TblCliente
-                                    where dni.Dni == CadenaDni
-                                    select dni;
+                        operacion.BuscarDni();
 
-                    foreach (var item in searchDNI)
-                    {
-                         
-                        Console.WriteLine(item.Dni+" "+item.NombreCliente);
-                    }
-
-                        break;
-                case '2':
+                    break;
+                case 2:
                     Console.WriteLine("Vamos");
                     break;
-                case '3':
+                case 3:
                     Console.WriteLine("Asi esta bien");
                     break;
-                case '4':
+                case 4:
                     Console.WriteLine("Terminamos");
                     break;
                 default:
